5a87e1cde423b4d36a4208f7184e05c805984857
Kalu Patrick <Kalupatrick94@gmail.com>
2021-11-04 10:07:21 +0100

    jobsApi

---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const { login, register, getUsers } = require("../controllers/auth");
+
+router.get("/users", getUsers);
+router.post("/register", register);
+router.post("/login", login);
+
+module.exports = router;
